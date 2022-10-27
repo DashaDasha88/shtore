c9e02edb89efe25fcff1dd1bf79fde2214909989
Dasha <dasha.kotova.mail@gmail.com>
2022-10-26 21:14:24 -0400

    add express validator

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const { check } = require('express-validator');
+
 const usersRepo = require('../../respositories/users');
+const signUpTemplate = require('../../views/admin/auth/signup');
+const signInTemplate = require('../../views/admin/auth/signin');
 
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
-    res.send("o hai");
+    res.send(signUpTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', [
+    check('email'),
+    check('password'),
+    check('passwordConfirmation')
+]   , async (req, res) => {
     const {email, password, passwordConfirmation} = req.body;
 
     const existingUser = await usersRepo.getOneBy({ email });
@@ -35,7 +43,7 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) =>{
-
+    res.send(signInTemplate());
 });
 
 router.post('/signin', async (req, res) => {
