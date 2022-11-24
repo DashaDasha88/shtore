e7e8f87a285d91e4baee6e1b66b2eaf9b8330645
Dasha <dasha.kotova.mail@gmail.com>
2022-11-23 21:10:59 -0500

    add cart routes

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,44 @@
 const express = require('express');
+const cartsRepo = require('../respositories/carts');
+const productsRepo = require('../respositories/products');
 
 const router = express.Router();
+
+router.post('/cart/products', async (req, res) => {
+
+    let cart;
+
+    if (!req.session.cartId) {
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+
+    const existingItem = cart.items.find(item => item.id === req.body.productId);
+
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.items.push({ id: req.body.productId, quantity: 1 });
+    }
+
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    })
+
+});
+
+router.get('/cart', async (req, res) => {
+    if(!req.session.cartId) {
+        return res.redirect('/');
+    }
+
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    for (let item of cart.items) {
+        const product = await productsRepo.getOne(item.id);
+    }
+})
 
 module.exports = router;
